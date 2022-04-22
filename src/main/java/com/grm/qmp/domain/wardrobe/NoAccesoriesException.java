268bd5be7dcbea271c0a0b09621e820cf902d6c7
Guido Miguez <38133798+grmiguez@users.noreply.github.com>
2022-04-22 20:20:16 -0300

    Delete NoAccesoriesException.java

--- src/main/java/com/grm/qmp/domain/wardrobe/NoAccesoriesException.java
+++
@@ -1,9 +0,0 @@
-package com.grm.qmp.domain.wardrobe;
-
-public class NoAccesoriesException extends Exception {
-
-    public NoAccesoriesException() {
-        super("No hay accesorios en el guardarropas, por lo que no se puede generar un Atuendo");
-    }
-
-}
