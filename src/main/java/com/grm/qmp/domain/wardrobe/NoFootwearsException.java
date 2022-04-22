2eb81de1f0f69b0a6b665f249db5b3b6cb481067
Guido Miguez <38133798+grmiguez@users.noreply.github.com>
2022-04-22 20:20:32 -0300

    Delete NoFootwearsException.java

--- src/main/java/com/grm/qmp/domain/wardrobe/NoFootwearsException.java
+++
@@ -1,9 +0,0 @@
-package com.grm.qmp.domain.wardrobe;
-
-public class NoFootwearsException extends Exception {
-
-    public NoFootwearsException() {
-        super("No hay calzados en el guardarropas, por lo que no se puede generar un Atuendo");
-    }
-
-}
