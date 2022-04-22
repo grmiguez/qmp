e74c7c132d14ee952ea4533335e6ba4b43dc9869
Guido Miguez <38133798+grmiguez@users.noreply.github.com>
2022-04-22 20:20:24 -0300

    Delete NoLowerClothingsException.java

--- src/main/java/com/grm/qmp/domain/wardrobe/NoLowerClothingsException.java
+++
@@ -1,9 +0,0 @@
-package com.grm.qmp.domain.wardrobe;
-
-public class NoLowerClothingsException extends Exception {
-
-    public NoLowerClothingsException() {
-        super("No hay prendas inferiores en el guardarropas, por lo que no se puede generar un Atuendo");
-    }
-
-}
